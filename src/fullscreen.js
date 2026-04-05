81137fd69c8977e832b0afc453519ca48978b45b
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2026-04-05 08:15:58 +0200

    Expose 'app.fullscreenDeactivate' function

--- src/fullscreen.js
+++ src/fullscreen.js
@@ -85,6 +85,18 @@ const FullscreenControl = L.Control.extend({
       }
     })
 
+    app.fullscreenDeactivate = () => {
+      if (mode === 'screen') {
+        if (document.fullscreenElement) {
+          document.exitFullscreen()
+        }
+      }
+
+      document.body.classList.remove('fullscreen')
+
+      app.emit('fullscreen-deactivate', map)
+    }
+
     return container
   }
 })
